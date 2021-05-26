b3d6ea64daeb9dff4deb6f158ba697aab50cfa12
psk264 <priya.khodwe@gmail.com>
2021-05-25 20:21:17 -0400

    capture user input

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ _step 5: create container on using anaconda_
 _step 6: activate container_
 _step 7: run python game.py_
 _step 8: commit the changes to github_
+_step 9: push the changes to origin so its uploaded on the remote repository_
--- game.py
+++ game.py
@@ -1,3 +1,14 @@
 # game.py
 
-print("Rock, Paper, Scissors, Shoot!")
+#Input command variation, using print and input 
+# print("Enter your name: ")
+# player_name = input()
+
+#Above statements can be reduced to single line of code
+player_name = input("Enter your name: ")
+
+print("Welcome, ", player_name, "! Get ready to play Rock-Paper-Scissors.")
+
+user_choice = input("Please chose one of 'rock', 'paper', 'scissors': ")
+print(player_name,"'s choice:", user_choice)
+
