86f466c970947802b78114cb06ce926cd539c0c9
psk264 <priya.khodwe@gmail.com>
2021-05-25 20:52:56 -0400

    random computer choice

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 # game.py
 
+#import modules to use additional modules
+import random
+
 #Input command variation, using print and input 
 # print("Enter your name: ")
 # player_name = input()
@@ -7,12 +10,22 @@
 #Above statements can be reduced to single line of code
 player_name = input("Enter your name: ")
 
+#User Choice:
 print("Welcome, ", player_name, "! Get ready to play Rock-Paper-Scissors.")
-
 user_choice = input("Please chose one of 'rock', 'paper', 'scissors': ")
-
 print(player_name,"'s choice:", user_choice)
 
+#Computer Choice:
+# one way to write the choice statement is to enter the list in the argument
+# computer_choice = random.choice(["rock","paper","scissors"]) 
+
+#another way to write choice statement using list
+valid_options = ["rock","paper","scissors"]
+computer_choice = random.choice(valid_options) 
+
+print("Computer's choice:", computer_choice)
+
+#Game Logic
 if(user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
     print("VALID.KEEP GOING")
 else:
