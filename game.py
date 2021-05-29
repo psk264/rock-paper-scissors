aea41c7d987b1043c82bd5495766c387f5765b07
psk264 <priya.khodwe@gmail.com>
2021-05-29 14:41:05 -0400

    Update user-friendly message when playing from CMD prompt

--- game.py
+++ game.py
@@ -11,20 +11,27 @@ import dotenv
 
 #Above statements can be reduced to single line of code
 #Option 1:
+# print("Fetching player name from command line")
 # player_name = input("Enter your name: ")
 
-#Option 2: Fetching player name from command line using env (package needed: os)
+#Option 2: Fetching player name from command line using command: PLAYER_NAME="Guest" if env is missing (package needed: os)
 # PLAYER_NAME = os.getenv("PLAYER_NAME")
-# print(PLAYER_NAME)
 # player_name = PLAYER_NAME
+# print("Fetching player name from command line using command \'PLAYER_NAME=\"Guest\" python game.py\'")
+
 
 #Option 3:Fetching player name from environment variable (package needed: dotenv)
 dotenv.load_dotenv()
 player_name = os.getenv("PLAYER_NAME")
+print("Fetching player name from env file")
 
 
 #User Choice:
-print("Welcome, ", player_name, "! Get ready to play Rock-Paper-Scissors.")
+if(player_name and player_name.strip()):
+    print("Welcome, ", player_name, "! Get ready to play Rock-Paper-Scissors.")
+else:
+    print("OOPS! player name is empty.  No problem, we got your back!")
+    player_name = input("Please enter the player name: ")
 user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 print(player_name+"'s choice:", user_choice)
 
@@ -71,7 +78,8 @@ if(user_choice in valid_options):
     elif(user_choice == "rock" and computer_choice == "scissors") or (user_choice == "paper" and computer_choice == "rock") or (user_choice == "scissors" and computer_choice == "paper"):
         print(f"{player_name} won! {user_choice} beats {computer_choice}")
     else:    
-        print("Oh, the computer won. It's ok.")
+        print(f"Oh, the computer won! {computer_choice} beats {user_choice}")
+        print("It's ok")
     print("Thanks for playing. Please play again!")
 
 else:
